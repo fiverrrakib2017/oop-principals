e0be9552b70162d5078b9ea24bba135510a7f7d4
fiverrrakib2017 <fiverrrakib2017@gmail.com>
2023-09-07 18:55:17 +0600

    get data with singleton design pattern

--- SingleTon/index.php
+++ SingleTon/index.php
@@ -2,18 +2,47 @@
 
 class Database{
     protected static $instance=NULL;
+    private $connection; 
+
     public function __construct(){
-        echo "connect<br>";
+        try {
+            // Private constructor to prevent instantiation from outside
+            $this->connection = new mysqli("localhost", "root", "", "api_project");
+            
+            if ($this->connection->connect_error) {
+                throw new Exception("Connection failed: " . $this->connection->connect_error);
+            }
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
     }
+
     public static function connect(){
         if (isset(Database::$instance)==null) {
             self::$instance = new Database();
         }
         return Database::$instance;
     }
+
+    public  function fetchData(){
+        $sql = "SELECT * FROM students";
+        $result = $this->connection->query($sql);
+
+        // $con=Database::connect();
+        // $result =$con->query("SELECT * FROM students");
+
+        if ($result->num_rows > 0) {
+            // Output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "ID: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+    }
+
+
 }
 
-Database::connect();
-Database::connect();
-Database::connect();
-Database::connect();
+$db=Database::connect();
+$db->fetchData();
